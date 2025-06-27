1b9c3f403ab3b9f4311bcee59c303bef1fc2f3a3
PenguinBotOnGithub <penguinbotonline@protonmail.com>
2025-06-27 08:58:16 +0700

    feat: implement plain text and password input components

---
+++ resources/js/Components/PasswordInput.vue
@@ -0,0 +1,19 @@
+<template>
+    <label :for="inputName" class="floating-label">
+        <span>{{ inputName }}</span>
+        <input :name="inputName"
+            :class="[{ ['border-error']: inputError != null }, 'input', 'w-sm', 'w-md', 'focus:outline-none', 'rounded-lg']"
+            :placeholder="inputPlaceholder" v-model="inputValue" type="password" />
+        <div class="text-error text-sm">{{ inputError }}</div>
+    </label>
+</template>
+<script setup lang="js">
+import { defineProps } from 'vue';
+
+defineProps({
+    inputName: String,
+    inputPlaceholder: String
+});
+const inputValue = defineModel("input-value");
+const inputError = defineModel("input-error");
+</script>
