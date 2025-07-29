12fb9a616f8fd885d40d173d835c60a1594ed1bd
PenguinBotOnGithub <penguinbotonline@protonmail.com>
2025-07-29 14:54:33 +0700

    feat: update Surat page stub with add button

--- resources/js/Pages/Surat.vue
+++ resources/js/Pages/Surat.vue
@@ -1,11 +1,12 @@
 <template>
-    <div class="flex flex-col">
-        <h1>Surat Page</h1>
+    <div class="flex flex-col items-center justify-center grow">
+        <Link href="/surat/create" class="p-2 m-0 bg-primary rounded-md">Tambah Surat</Link>
     </div>
 </template>
 
 <script setup lang="js">
 import Shell from '../Components/Shell.vue';
+import { Link } from '@inertiajs/vue3';
 
 defineOptions({ layout: Shell });
 </script>
